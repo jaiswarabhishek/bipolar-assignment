01f2a05739f67fb87bf7b2b82032cfb9144417eb
Abhishek <jangal&mangal.gov>
2024-01-16 11:49:36 +0530

    Form Component Done

---
+++ src/components/Forms.jsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MenuItem from '@mui/material/MenuItem';
+
+const currencies = [
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'BTC',
+    label: '฿',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+
+const defaultTheme = createTheme();
+
+export default function Forms() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="lg">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            border: '0px solid black',
+            padding: '30px 42px 60px 42px',
+            boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+            borderRadius: '16px',
+            width:'40%',
+            marginLeft:'auto',
+            marginRight:'auto',
+          }}
+        >
+          
+          <Typography  sx={{marginBottom:'20px',fontSize:'40px',fontWeight:'700'}}>
+            XYZ
+          </Typography>
+            <Typography sx={{marginBottom:'5px',fontSize:'26px',fontWeight:'400'}}>
+          Welcome Onboard
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+           <TextField id="filled-basic"  sx={{width:'100%',marginBottom:'20px',backgroundColor: '#E7E0EC',borderRadius: '4px',}} label="Your Name" variant="filled" />
+           <TextField id="filled-basic" sx={{width:'100%',marginBottom:'20px',backgroundColor: '#E7E0EC',borderRadius: '4px',}} label="Company Name" variant="filled" />
+           <TextField
+          id="filled-select-currency"
+          select
+          variant="filled"
+          label='Select Industry'
+          sx={{width:'100%',backgroundColor: '#E7E0EC',borderRadius: '4px',}}
+         />
+          
+        
+            
+            <Typography
+            sx={{
+                marginTop: '20px',
+                marginBottom: '10px',
+                color: '#333333',
+                fontSize: '16px',
+                fontWeight: '600'
+            
+            }}
+            >
+                Select Number of Locations
+            </Typography>
+            <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                marginTop: '10px',
+                width: '85%',
+            }}
+            >
+                <Box
+                sx={{
+                    border: '0px solid #333333',
+                    borderRadius: '4px',
+                    backgroundColor: '#E7E0EC',
+                    padding: '5px 10px',
+                    color: '#222222'
+                }}
+                >1-10</Box>
+                <Box
+                sx={{
+                    border: '0px solid #333333',
+                    borderRadius: '4px',
+                    backgroundColor: '#E7E0EC',
+                     padding: '5px 10px',
+                    color: '#222222'
+                }}
+                >10-50</Box>
+                <Box
+                sx={{
+                    border: '0px solid #333333',
+                    borderRadius: '4px',
+                    backgroundColor: '#333333',
+                     padding: '5px 10px',
+                    color: 'white'
+                }}
+                >50-150</Box>
+                <Box
+                sx={{
+                    border: '0px solid #333333',
+                    borderRadius: '4px',
+                    padding: '5px 10px',
+                    backgroundColor: '#E7E0EC',
+                    color: '#222222'
+                }}
+                >150+</Box>
+            </div>
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 , backgroundColor:'#333333',borderRadius:'8px' ,padding: '10px 40px'}}
+            >
+             CONTINUE
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,6 @@ import Cards from "@/components/Cards";
 
 export default function Home() {
   return (
-      <Cards />
+      <Forms />
   );
 }
